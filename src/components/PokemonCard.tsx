359f92d8ca4828bc60966e76b89c5ec92661d32b
Clément Praud <clement.praud6@gmail.com>
2024-10-22 11:10:24 +0200

    Add Dragapult card and caption

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,5 @@
+function PokemonCard() {
+  return <figure><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/887.png" alt="lanssorien - type spectre/dragon" /><figcaption>Dragapult</figcaption></figure>;
+}
+
+export default PokemonCard;
