535cd76725cedd939fd4cb8ef611e34977c6492a
Clément Praud <clement.praud6@gmail.com>
2024-10-22 12:18:42 +0200

    Make the app dynamic

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,21 @@
 function PokemonCard() {
-  return <figure><img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/887.png" alt="lanssorien - type spectre/dragon" /><figcaption>Dragapult</figcaption></figure>;
+  let pokemon = pokemonList[1];
+  let source = pokemon.imgSrc;
+  return (<figure>
+    <p>{source !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : "???"}</p>
+    <figcaption alt={pokemon.name}>{pokemon.name}</figcaption>
+    </figure>);
 }
+
+const pokemonList = [
+  {
+    name: "Dragapult",
+    imgSrc:
+      "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/887.png",
+  },
+  {
+    name: "Mew",
+  },
+];
 
 export default PokemonCard;
